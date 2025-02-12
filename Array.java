66765d1bc3db3ca4b6b4fc7af53b812e9a440fac
Azael Rosales <mogurirosales@hotmail.com>
2025-02-12 11:15:00 -0600

    Fix language

--- Array.java
+++ Array.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el tamaño del arreglo");
+        System.out.println("Whats the size of the array");
             int tam =sc.nextInt();
             int array[] = new int [tam];
-        System.out.println("Ingrese los elementos del arreglo");
+        System.out.println("Enter the numbers of the array");
 
         for(int i =0; i<tam; i++){
-        System.out.println("elemento " +(i+1) +":");
+        System.out.println("Number " +(i+1) +":");
         array[i] = sc.nextInt();
 
    }
    for(int l=0; l<tam; l++){
-    System.out.println("El arreglo queda de la siguiente forma " +array[l]);
+    System.out.println("This is your array: " +array[l]);
    }
    sc.close();
 }
